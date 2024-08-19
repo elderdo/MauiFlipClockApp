8898a9038f04ea6895723adccae23df0da97de6b
Douglas S Elder <elderdo@yahoo.com>
2024-08-19 10:56:36 -0500

    initial rev

---
+++ MauiFlipClock/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiFlipClock;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ MauiFlipClock/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiFlipClock;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
