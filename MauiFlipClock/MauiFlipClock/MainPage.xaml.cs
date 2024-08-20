f55e68c765fe893fece89e4ce3544ae5104f2873
Douglas S Elder <elderdo@yahoo.com>
2024-08-19 21:39:11 -0500

    initial rev

---
+++ MauiFlipClock/MauiFlipClock/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace MauiFlipClock;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+        Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+        {
+            var currentTime = DateTime.Now;
+            Digit1.Text = currentTime.Hour.ToString("D2")[0].ToString();
+            Digit2.Text = currentTime.Hour.ToString("D2")[1].ToString();
+            Digit3.Text = currentTime.Minute.ToString("D2")[0].ToString();
+            Digit4.Text = currentTime.Minute.ToString("D2")[1].ToString();
+            return true;
+        });
+    }
+
+}
+
